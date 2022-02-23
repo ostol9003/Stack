8982088645509fb09d92ff1e693fae98db5779ca
OstolskiM <ostolski.marcin@gmail.com>
2022-02-23 22:38:43 +0100

    Initial Commit

---
+++ Stack/main.cpp
@@ -0,0 +1,82 @@
+//
+//  main.cpp
+//  Stack
+//
+//  Created by Marcin Ostolski on 23/02/2022.
+//
+
+#include <iostream>
+using namespace std;
+
+const int LIMIT = 30;
+typedef int item_t;
+struct Stack
+{
+    int tab[LIMIT];
+    item_t top;
+};
+
+void create(Stack &s){
+   s.top = -1;
+}
+
+int isEmpty(Stack s){
+  if(s.top == -1) return 1;
+  return 0;
+
+  //return (s.top == -1);
+
+}
+
+int isFull(Stack s){
+  if(s.top==LIMIT-1)return 1;
+  return 0;
+}
+
+void push(Stack &s, item_t x){
+  if (!isFull(s))
+  {
+    s.top=s.top+1;
+    s.tab[s.top]=x;
+  }
+}
+
+item_t pop(Stack &s){
+ if (!isEmpty(s))
+  {
+    s.top=s.top-1;
+    return s.tab[s.top+1];
+  }
+  else
+    return -1000;
+}
+
+item_t top(Stack &s){
+ if (!isEmpty(s))
+  {
+    return s.tab[s.top];
+  }
+  else
+    return -1000;
+}
+
+
+int main()
+{
+    Stack s;
+    create(s);
+    int x;
+    x = pop(s);
+    cout << x <<endl;
+    push(s, 11);
+    push(s, 55);
+    push(s, 31);
+    push(s, -15);
+    cout << "Na szczycie stosu jest " << top(s) <<endl;
+    cout << "Operacja pop: " << pop(s) << endl;
+    cout << "Operacja pop: " << pop(s) << endl;
+    cout << "Na szczycie stosu jest " << top(s) <<endl;
+
+    //...
+    return 0;
+}
